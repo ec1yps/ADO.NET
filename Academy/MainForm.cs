2702060312fd097271069a7ad3f88b4112733aa7
ec1yps_ <Mihli_sk06@top-academy.ru>
2025-04-15 22:19:38 +0300

    Dependencies almost DONE TODO: AllGroups in ComboBox;

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -20,6 +20,8 @@ namespace Academy
 		public Dictionary<string, int> d_directions;
 		public Dictionary<string, int> d_groups;
 
+		public Dictionary<ComboBox, List<ComboBox>> d_dependencies;
+
 		DataGridView[] tables;
 		Query[] queries = new Query[]
 			{
@@ -64,6 +66,11 @@ namespace Academy
 		public MainForm()
 		{
 			InitializeComponent();
+
+			d_dependencies = new Dictionary<ComboBox, List<ComboBox>>
+			{
+				{ cbStudentsDirection, new List<ComboBox>(){ cbStudentsGroup } }
+			};
 
 			tables = new DataGridView[]
 				{
@@ -198,16 +205,23 @@ namespace Academy
             int i = (sender as ComboBox).SelectedIndex;
 
 			#region Filtercb_StudentsGroup
-				Dictionary<string, int> d_groups = connector.GetDictionary
-							(
-								"group_id,group_name",
-								"Groups",
-								i == 0 ? "" : $"{cb_suffix.ToLower()}={dictionary[(sender as ComboBox).SelectedItem.ToString()]}"
-							);
+				/*Dictionary<string, int> d_groups = connector.GetDictionary
+					(
+						"group_id,group_name",
+						"Groups",
+						i == 0 ? "" : $"{cb_suffix.ToLower()}={dictionary[(sender as ComboBox).SelectedItem.ToString()]}"
+					);
 
 				cbStudentsGroup.Items.Clear();
 				cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());
-				cbStudentsGroup.Items.Insert(0, "Все группы"); 
+				cbStudentsGroup.Items.Insert(0, "Все группы"); */
+			if(d_dependencies.ContainsKey(sender as ComboBox))
+			{
+				foreach (ComboBox cb in d_dependencies[sender as ComboBox])
+				{
+					GetDependentData(cb, sender as ComboBox);
+				}
+			}
 			#endregion
 
 			Query query = new Query(queries[tabControl.SelectedIndex]);
@@ -235,5 +249,32 @@ namespace Academy
 				
 			LoadPage(tabControl.SelectedIndex, query);
 		}
+
+		void GetDependentData(ComboBox dependent, ComboBox determinant)
+		{
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine(dependent.Name + "\t" + determinant.Name);
+
+			string dependent_root = 
+				dependent.Name.Substring(Array.FindLastIndex<char>(dependent.Name.ToCharArray(), Char.IsUpper));
+			string determinant_root = 
+				determinant.Name.Substring(Array.FindLastIndex<char>(determinant.Name.ToCharArray(), Char.IsUpper));
+
+			Dictionary<string, int> dictionary = 
+				connector.GetDictionary
+				(
+					$"{dependent_root.ToLower()}_id,{dependent_root.ToLower()}_name",
+					$"{dependent_root}s,{determinant_root}s",
+					determinant.SelectedIndex == 0 ? "" : $"{determinant_root.ToLower()}={determinant.SelectedIndex}"
+				);
+			
+			dependent.Items.Clear();
+			dependent.Items.AddRange(dictionary.Select(d => d.Key).ToArray());
+			dependent.Items.Insert(0, "Все группы");
+
+			Console.WriteLine("Dependent:\t" + dependent_root);
+            Console.WriteLine("Determinant:\t" + determinant_root);
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
 	}
 }
